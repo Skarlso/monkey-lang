8a5d01a6b16372ff44604a05a0bf07d652343d1b
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2024-09-22 17:10:08 +0200

    adding the first pieces of an AST and a parser

---
+++ ast/ast.go
@@ -0,0 +1,49 @@
+package ast
+
+import "github.com/Skarlso/horcsog/token"
+
+type Node interface {
+	// TokenLiteral is used for debugging purposes.
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	// dummy methods to signify that we are dealing with a Statement
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	//
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+
+	return ""
+}
+
+type LetStatement struct {
+	Token token.Token // the token.LET Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
