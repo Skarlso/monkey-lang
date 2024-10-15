88091acd6284274e0328d9fbe19a9c5dff43a7c4
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2024-10-15 22:30:25 +0200

    adding the E into REPL

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/Skarlso/went/evaluator"
 	"github.com/Skarlso/went/lexer"
 	"github.com/Skarlso/went/parser"
 )
@@ -32,8 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 
 		// for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 		// 	fmt.Fprintf(out, "%+v\n", tok)
